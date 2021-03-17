8c48f063a6037974f2436b1fa5a6d8b66a15673a
Marty <martysteer@gmail.com>
2021-03-17 21:11:43 +0000

    download george orwell ;-)

---
+++ websites/www.george-orwell.org/~sub/search.php
@@ -0,0 +1,163 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<title>Searching error</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="http://www.shakespeare-literature.com/~sub/style.css" rel="stylesheet" type="text/css">
+<script language="JavaScript" type="text/JavaScript">
+<!--
+function MM_reloadPage(init) {  //reloads the window if Nav4 resized
+  if (init==true) with (navigator) {if ((appName=="Netscape")&&(parseInt(appVersion)==4)) {
+    document.MM_pgW=innerWidth; document.MM_pgH=innerHeight; onresize=MM_reloadPage; }}
+  else if (innerWidth!=document.MM_pgW || innerHeight!=document.MM_pgH) location.reload();
+}
+MM_reloadPage(true);
+//-->
+</script>
+</head>
+<body background="http://www.mtwain.com/~sub/bg2.gif" topmargin="0" leftmargin="0" bottommargin="0">
+<table width="780" border="0" cellpadding="3" cellspacing="0" bgcolor="#FFFFFF">
+  <tr>
+    <td valign="top"> <table width="100%" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <td valign="top"> <table width="100%" border="0" cellpadding="0" cellspacing="0">
+              <tr>
+                <td width="7%" valign="top"> <img src="http://www.mtwain.com/~sub/book2.gif" width="67" height="50"></td>
+                <td width="93%" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                      <td width="65%" valign="top"> <p class="container"><strong><font size="3">The Complete Works of</font></strong>
+                          <font size="3" class="text"><strong>The Complete Works of</strong></font><strong><font color="#B0B0B0" size="6">George-Orwell&nbsp;&nbsp;&nbsp;&nbsp;</font></strong></td>
+                      <td width="35%" valign="middle"> <div align="left"><font size="1"><img src="spacer.gif" width="1" height="13"><br>
+                          [http://www.george-orwell.org]</font></div></td>
+                    </tr>
+                  </table>
+                  <hr align="right" width="700" size="1" noshade> </td>
+              </tr>
+            </table></td>
+        </tr>
+        <tr>
+          <td valign="top"> <table width="705" border="0" align="right" cellpadding="0" cellspacing="0">
+              <tr>
+                <td height="12" bgcolor="#E6E6E6"> <div align="center"><font size="1">[<a href="http://www.george-orwell.org/l_biography.html" title="George Orwell Biography">GEORGE ORWELL BIOGRAPHY</a> <strong>|</strong> <a href="http://www.george-orwell.org/l_pictures.html" title="George Orwell Pictures">PICTURES</a> <strong>|</strong> <a href="http://www.george-orwell.org/l_quotes.html" title="George Orwell Quotes">QUOTES</a> <strong>|</strong> <a href="http://www.george-orwell.org/l_articles" title="Related george orwell articles">RELATED ARTICLES</a>]
+</font></div></td>
+              </tr></p>
+            </table></td>
+        </tr>
+        <tr>
+          <td valign="top"></td>
+        </tr>
+      </table>
+      <table width="100%" border="0">
+        <tr>
+          <td width="7%" valign="top">&nbsp;</td>
+          <td width="77%" valign="top" bgcolor="#FFFFFF"> <table width="100%" border="0" cellpadding="0" cellspacing="2">
+
+              <tr>
+                <form name="form1" method="post" action=""><td width="100%" valign="bottom"> <table width="100%" border="0" cellpadding="0" cellspacing="0">
+                      <tr>
+                      <td width="43%"><font size="1">
+&nbsp;
+                       </font></td>
+                        <td width="57%"> <div align="right">
+                        	<form action="http://sub.mtwain.com/search.php" method="GET">
+                            <input type="text" name="query" class="forms_field">
+                            <select name="book" size="1" class="forms_dropdown">
+                              <option value="0">Select</option>
+							  <option value="188" >1984</option>34<option value="193" >A Clergyman's Daughter</option>52<option value="190" >Animal Farm</option>41<option value="194" >Burmese Days</option>42<option value="191" >Coming up for Air</option>47<option value="195" >Down and Out in Paris and London</option>62<option value="192" >Keep the Aspidistra Flying</option>56<option value="199" >Homage to Catalonia</option>49<option value="198" >The Road to Wigan Pier</option>52                            </select>
+                            <input name="Search" value="SEARCH" type="submit" class="forms_submit">
+                          </form>
+                          </div>
+                        </td>
+                    </tr>
+                  </table></td></form>
+              </tr>
+              <tr>
+                <td bgcolor="#FFFFFF"></td>
+              </tr>
+              <tr>
+                <td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                      <td valign="top" bgcolor="#B63D3D"> <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                          <tr>
+                            <td><table width="100%" border="0" cellspacing="1" cellpadding="0">
+                                <tr>
+                                  <td bgcolor="white"> <table width="100%" border="0" cellpadding="5" cellspacing="0">
+                                      <tr>
+                                        <td height="21" valign="top" bgcolor="#E6E6E6">
+                                          <p class="container"><i><strong><font size="4">
+                                          [
+												Search Results...
+
+                                          ]</font></strong><font size="4" class="text">
+                                            <strong>
+                                            [
+												Search Results...
+                                            ]</strong></font></i></p></td>
+                                      </tr>
+                                      <tr>
+                                        <td><p align="left">
+
+                                          <p align="left"><font size="2">Sorry, the provided search query was too short</font></p>
+                                          <p>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          <br><br><br>
+                                          </p></td>
+                                      </tr>
+                                    </table></td>
+                                </tr>
+                              </table></td>
+                          </tr>
+                        </table></td>
+                    </tr>
+                  </table></td>
+                <br>
+                <td width="14%" valign="top" bgcolor="#E6E6E6">
+                &nbsp;
+                </td>
+              </tr>
+            </table></td>
+        </tr>
+      </table></td>
+  </tr>
+  <tr>
+    <td height="93" valign="top"> <table width="100%" border="0">
+        <tr>
+          <td valign="top"> <table width="705" border="0" align="right">
+              <tr>
+                <td>
+                <hr align="right" width="700" size="1" noshade>
+                <div align="left"><strong><font size="1">Copyright 2003, mtwain.com</font></strong><br>
+                    <strong><font size="1">Contact the <a href="http://www.george-orwell.org/contact.html">webmaster</a></font></strong><br>
+                    <strong><font size="1">Disclaimer <a href="http://www.george-orwell.org/disclaimer.html">here</a>. Privacy Policy <a href="http://www.george-orwell.org/privacypolicy.html">here</a>.</font></strong><br>
+                  </div></td>
+              </tr>
+            </table></td>
+        </tr>
+      </table></td>
+  </tr>
+  <tr>
+    <td> </td>
+  </tr>
+</table>
+</body>
+</html>
+
